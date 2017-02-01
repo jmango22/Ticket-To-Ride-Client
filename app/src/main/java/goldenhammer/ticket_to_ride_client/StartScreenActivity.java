6b886ac65275d86977cbb5aa0463bfa074f1d2af
Jon Meng <jonathonmeng@gmail.com>
2017-02-01 12:28:46 -0700

    Initial Commit

---
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/StartScreenActivity.java
@@ -0,0 +1,13 @@
+package goldenhammer.ticket_to_ride_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
