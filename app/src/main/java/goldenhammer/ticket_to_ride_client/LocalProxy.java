0186b4519c61983db536f05c6c3d7ab04cd296d9
Jon Meng <jonathonmeng@gmail.com>
2017-02-04 11:07:32 -0700

    moved proxy files to the right location

---
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/LocalProxy.java
@@ -0,0 +1,25 @@
+package edu.goldenhammer.ticket_to_ride_client;
+
+import java.util.List;
+
+class LocalProxy implements IProxy{
+    @Override
+    public boolean login(String username, String password) {
+        return true;
+    }
+
+    @Override
+    public boolean register(String username, String password) {
+        return true;
+    }
+
+    @Override
+    public List<String> getPlayerGames(String username) {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllGames() {
+        return null;
+    }
+}
---
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/ServerProxy.java
@@ -0,0 +1,25 @@
+package goldenhammer.ticket_to_ride_client;
+
+import java.util.List;
+
+public class ServerProxy implements IProxy {
+    @Override
+    public boolean login(String username, String password) {
+        return false;
+    }
+
+    @Override
+    public boolean register(String username, String password) {
+        return false;
+    }
+
+    @Override
+    public List<String> getPlayerGames(String username) {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllGames() {
+        return null;
+    }
+}
