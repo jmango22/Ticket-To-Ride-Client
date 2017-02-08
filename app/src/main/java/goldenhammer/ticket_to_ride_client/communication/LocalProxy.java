f3222073228e536a3113854f1b04b9b1672dcee5
Jonathon Meng <mengje@mtc.byu.edu>
2017-02-08 09:03:21 -0700

    New proxy changes

--- app/src/main/java/goldenhammer/ticket_to_ride_client/communication/LocalProxy.java
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/communication/LocalProxy.java
@@ -2,6 +2,9 @@ package goldenhammer.ticket_to_ride_client.communication;
 
 import java.util.List;
 
+import goldenhammer.ticket_to_ride_client.model.Password;
+import goldenhammer.ticket_to_ride_client.model.Username;
+
 class LocalProxy implements IProxy{
     public static final LocalProxy SINGLETON = new LocalProxy();
 
@@ -11,22 +14,42 @@ class LocalProxy implements IProxy{
     public void setURL(String serverHost, String serverPort) {    }
 
     @Override
-    public boolean login(String username, String password) {
+    public boolean login(Username username, Password password) {
         return true;
     }
 
     @Override
-    public boolean register(String username, String password) {
+    public boolean register(Username username, Password password) {
         return true;
     }
 
     @Override
-    public List<String> getPlayerGames(String username) {
+    public List<String> getPlayerGames(Username username) {
         return null;
     }
 
     @Override
     public List<String> getAllGames() {
         return null;
+    }
+
+    @Override
+    public boolean createGame(String gameName) {
+        return false;
+    }
+
+    @Override
+    public boolean joinGame(String gameName) {
+        return false;
+    }
+
+    @Override
+    public void playGame(String gameName) {
+
+    }
+
+    @Override
+    public boolean leaveGame(String gameName) {
+        return false;
     }
 }
