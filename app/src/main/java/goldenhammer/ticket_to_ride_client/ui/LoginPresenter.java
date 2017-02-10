f8302ceccf254e6d5171eed4327857af75d9679a
Jon Meng <jonathonmeng@gmail.com>
2017-02-10 12:28:45 -0700

    fixed merge conflicts

---
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/ui/LoginPresenter.java
@@ -0,0 +1,75 @@
+package goldenhammer.ticket_to_ride_client.ui;
+
+import android.support.v4.util.Pair;
+
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+import goldenhammer.ticket_to_ride_client.communication.ServerProxy;
+import goldenhammer.ticket_to_ride_client.model.ClientModelFacade;
+import goldenhammer.ticket_to_ride_client.model.Password;
+import goldenhammer.ticket_to_ride_client.model.Username;
+
+/**
+ * Created by McKean on 2/3/2017.
+ */
+
+public class LoginPresenter implements Observer {
+    private LoginActivity owner;
+    private String portNum;
+    private String hostNum;
+
+    public LoginPresenter(LoginActivity loginActivity){
+        owner = loginActivity;
+        ClientModelFacade.SINGLETON.addObserver(this);
+    }
+    @Override
+    public void update(Observable o, Object arg) {
+        if (ClientModelFacade.SINGLETON.getUser() != null){
+            owner.onLogin(ClientModelFacade.SINGLETON.getUser().getUsername().getString());
+        }
+    }
+    public String getPortNum() {
+        return portNum;
+    }
+
+    public void setPortNum(String portNum) {
+        this.portNum = portNum;
+    }
+
+    public String getHostNum() {
+        return hostNum;
+    }
+
+    public void setHostNum(String hostNum) {
+        this.hostNum = hostNum;
+    }
+
+    public void sendLogin(String username, String password) {
+        Pair<Username,Password> credentials = check(username, password);
+        if (credentials != null){
+            ServerProxy.SINGLETON.login(credentials.first, credentials.second);
+        }
+    }
+
+    private Pair<Username,Password> check(String username, String password){
+        try{
+            Username u = new Username(username);
+            Password p = new Password(password);
+            return new Pair<>(u,p);
+        }catch(IOException e){
+            String error = e.getMessage();
+            owner.toastMessage(error);
+        }
+        return null;
+    }
+
+    public void sendRegistration(String username, String password) {
+        Pair<Username, Password> credentials = check(username,password);
+        if (credentials != null){
+            ServerProxy.SINGLETON.register(credentials.first, credentials.second);
+        }
+    }
+
+}
