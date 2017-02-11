cd18940d04b73e56f91584ae507af9b660cb4913
Jon Meng <jonathonmeng@gmail.com>
2017-02-11 10:54:47 -0700

    updated recyclerviews

--- app/src/main/java/goldenhammer/ticket_to_ride_client/ui/GameSelectorActivity.java
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/ui/GameSelectorActivity.java
@@ -2,8 +2,13 @@ package goldenhammer.ticket_to_ride_client.ui;
 
 import goldenhammer.ticket_to_ride_client.R;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.LinearLayoutManager;
+
 
 public class GameSelectorActivity extends AppCompatActivity {
 
@@ -11,9 +16,25 @@ public class GameSelectorActivity extends AppCompatActivity {
     //TODO : setAvailableGames that takes a List of All Games
     //TODO : setMyGames that takes a List of My Games
 
+    RecyclerView allGames = (RecyclerView)findViewById(R.id.allGamesRecycler);
+    RecyclerView myGames = (RecyclerView)findViewById(R.id.myGamesRecycler);
+
+    LinearLayoutManager llm = new LinearLayoutManager(getBaseContext());
+
+    public void toastMessage(String message) {
+        Context context = this.getApplicationContext();
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_selector);
+
+        allGames.setLayoutManager(llm);
+        myGames.setLayoutManager(llm);
     }
+
+
 }
