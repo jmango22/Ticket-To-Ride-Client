a3908b804fcf384460ba5ffa45b9492804185d22
mckrieger <mckean.krieger@byu.edu>
2017-02-21 22:35:53 -0700

    JavaDoc

--- app/src/main/java/goldenhammer/ticket_to_ride_client/ui/login/MyGamesPresenter.java
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/ui/login/MyGamesPresenter.java
@@ -13,21 +13,37 @@ import goldenhammer.ticket_to_ride_client.model.GameName;
  * Created by McKean on 2/6/2017.
  */
 
+/**
+ * MyGamesPresenter is responsible for conveying information to the MyGamesActivity, which shows a
+ * list of the games that contain the current user as a player. This presenter is updated as the
+ * model classes change. The presenter also calls methods in the serverProxy whenever the user
+ * initiates and action.
+ * @invariant The activity calling the presenter is the same as the variable owner.
+ * @invariant This presenter is an observer of the ClientModelFacade
+ * @invariant the proxy variable is some class that implements IProxy
+ */
 public class MyGamesPresenter implements Observer, IGameSelectorPresenter {
     private GameSelectorActivity owner;
     private IProxy proxy;
 
-
+    /**
+     * @pre The presenter is created by an instance of GameSelectorActivity, which is passed in
+     * as a parameter (not null);
+     * @post a new MyGamesPresenter is initialized and able to call appropriate methods.
+     * @param activity The activity that is creating the presenter. This is used to call
+     *                 methods in the activity after information is updated.
+     */
     public MyGamesPresenter(GameSelectorActivity activity){
         owner = activity;
         proxy = ServerProxy.SINGLETON;
         ClientModelFacade.SINGLETON.addObserver(this);
     }
 
-    public void getMyGames(){
-        ServerProxy.SINGLETON.getPlayerGames();
-    }
-
+    /**
+     * @pre the string name is not null
+     * @post either calls the proxy's command to create a game or creates a warning toast.
+     * @param name the desired name of the new game.
+     */
     public void createGame(String name){
         try{
             GameName g = new GameName(name);
@@ -38,6 +54,11 @@ public class MyGamesPresenter implements Observer, IGameSelectorPresenter {
         }
     }
 
+    /**
+     * @pre the observable object o was changed and called the update method properly.
+     * @param o Observable object that was changed.
+     * @param arg argument of what was changed.
+     */
     @Override
     public void update(Observable o, Object arg) {
         owner.setMyGameList(ClientModelFacade.SINGLETON.getMyGames().getAllGames());
@@ -47,6 +68,12 @@ public class MyGamesPresenter implements Observer, IGameSelectorPresenter {
         }
     }
 
+    /**
+     * @pre The string gameName is not null
+     * @post either calls the serverProxy's function to join the designated game or creates a
+     * toast if the string is not the proper format.
+     * @param gameName the name of the game to be joined.
+     */
     public void joinGame(String gameName){
         try {
 
@@ -56,6 +83,13 @@ public class MyGamesPresenter implements Observer, IGameSelectorPresenter {
             owner.toastMessage(e.getMessage());        }
     }
 
+    /**
+     *
+     * @pre the gameName is not null.
+     * @post calls the serverProxy's function to leave the designated game or creates a toast
+     * if the gameName is not the proper format.
+     * @param gameName the name of the game to leave.
+     */
     public void leaveGame(String gameName){
         try {
             String results = proxy.leaveGame(new GameName(gameName));
@@ -65,6 +99,11 @@ public class MyGamesPresenter implements Observer, IGameSelectorPresenter {
         }
     }
 
+    /**
+     * @pre the gameName is not null
+     * @post either calls the serverProxy's playGame method to start the designated game
+     * @param gameName the name of the game to be played.
+     */
     public void playGame(String gameName){
         try {
             String results = proxy.playGame(new GameName(gameName));
