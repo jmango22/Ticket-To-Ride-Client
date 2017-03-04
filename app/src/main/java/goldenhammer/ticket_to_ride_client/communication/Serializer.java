6a4d76a85c4f9ffa5cbe4a9ee2a54c951dd89394
Devon Kinghorn <devonkinghorn@iPhone-010024065205.app.byu.edu>
2017-03-04 12:57:03 -0700

    added serializer and command manager

---
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/communication/Serializer.java
@@ -0,0 +1,69 @@
+package goldenhammer.ticket_to_ride_client.communication;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import goldenhammer.ticket_to_ride_client.model.commands.Command;
+
+/**
+ * Created by devonkinghorn on 3/4/17.
+ */
+
+public class Serializer {
+    public static String serialize(Object c) {
+        Gson gson = new Gson();
+        return gson.toJson(c);
+    }
+    public static JsonObject deserialize(String json) {
+        JsonParser parser = new JsonParser();
+        return parser.parse(json).getAsJsonObject();
+    }
+    static final String commandPackagePrefix = "edu.goldenhammer.ticket_to_ride_client.model.commands";
+    public static Command deserializeCommand(String json) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+        String commandName = jsonObject.get("name").getAsString();
+        StringBuilder sb = new StringBuilder(commandName);
+        commandName = sb.replace(0, 1, sb.substring(0, 1).toUpperCase()).toString();
+        String className = commandPackagePrefix + commandName + "Command";
+        Command basecmd = null;
+        try {
+            Class c = null;
+            try {
+                c = Class.forName(className);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            basecmd = (Command)gson.fromJson(json, c);
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+        }
+        return basecmd;
+    }
+
+
+    public static List<Command> deserializeCommands(String commandString) {
+        Gson gson = new Gson();
+        JsonArray commands = gson.fromJson(commandString, JsonArray.class);
+        List<Command> list = new ArrayList<>();
+        for(int i = 0; i < commands.size(); i++) {
+            try {
+                Command c = deserializeCommand(commands.get(i).getAsString());
+                list.add(c);
+            } catch (JsonSyntaxException e){
+                e.printStackTrace();
+            }
+        }
+        return list;
+    }
+}
--- app/src/main/java/goldenhammer/ticket_to_ride_client/ui/play/GamePlayPresenter.java
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/ui/play/GamePlayPresenter.java
@@ -1,13 +1,21 @@
 package goldenhammer.ticket_to_ride_client.ui.play;
 
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import goldenhammer.ticket_to_ride_client.communication.Callback;
 import goldenhammer.ticket_to_ride_client.communication.IProxy;
+import goldenhammer.ticket_to_ride_client.communication.Results;
+import goldenhammer.ticket_to_ride_client.communication.Serializer;
 import goldenhammer.ticket_to_ride_client.communication.ServerProxy;
 import goldenhammer.ticket_to_ride_client.model.ClientModelFacade;
+import goldenhammer.ticket_to_ride_client.model.DestCard;
+import goldenhammer.ticket_to_ride_client.model.Track;
+import goldenhammer.ticket_to_ride_client.model.commands.Command;
+import goldenhammer.ticket_to_ride_client.model.commands.ReturnDestCardsCommand;
 
 /**
  * Created by devonkinghorn on 2/22/17.
@@ -16,10 +24,12 @@ import goldenhammer.ticket_to_ride_client.model.ClientModelFacade;
 public class GamePlayPresenter implements Observer, IGamePlayPresenter {
     private GamePlayActivity owner;
     private IProxy proxy;
+    private ClientModelFacade model;
     public GamePlayPresenter(GamePlayActivity activity) {
         owner = activity;
         proxy = ServerProxy.SINGLETON;
-        ClientModelFacade.SINGLETON.addObserver(this);
+        model = ClientModelFacade.SINGLETON;
+        model.addObserver(this);
     }
 
     @Override
@@ -28,17 +38,28 @@ public class GamePlayPresenter implements Observer, IGamePlayPresenter {
     }
 
     @Override
-    public void takeTrainCard() {
+    public void takeTrainCards() {
 
     }
 
     @Override
-    public void takeDestCard() {
+    public void takeDestCards() {
 
     }
 
     @Override
-    public void takeTrack() {
+    public void returnDestCards(List<DestCard> toReturn) {
+        proxy.doCommand(model.getCurrentGame().getGameName(), new ReturnDestCardsCommand(toReturn), new Callback() {
+            @Override
+            public void run(Results res) {
+                List<Command> commands = Serializer.deserializeCommands(res.getBody());
+                //TODO: tell the facade to take the new commands.
+            }
+        });
+    }
+
+    @Override
+    public void takeTrack(Track track) {
 
     }
 
