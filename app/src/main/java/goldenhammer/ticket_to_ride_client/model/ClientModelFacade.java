232465d5fbe324f424298dc27a0a30b749e220da
Jon Meng <jonathonmeng@gmail.com>
2017-03-04 10:18:18 -0700

    Updated the Model while doing some Tests.

--- app/src/main/java/goldenhammer/ticket_to_ride_client/model/ClientModelFacade.java
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/model/ClientModelFacade.java
@@ -12,15 +12,6 @@ import java.util.Observer;
  * JavaDoc created by Jonathon Meng 2/22/2017
  */
 
-/**
- * The ClientModelFacade provides a way to access the Games Client model through its singleton
- * pattern. This class keeps track of the game all the games available, as well as the games
- * that the player is a part of. This class handles all access to the Player class as well.
- * @invariant class follows the SINGLETON pattern
- * @invariant class extends Observable
- */
-
-
 public class ClientModelFacade extends Observable {
     private GameList mAvailableGames = new GameList(new ArrayList<GameListItem>());
     private GameList mMyGames= new GameList(new ArrayList<GameListItem>());
@@ -32,104 +23,50 @@ public class ClientModelFacade extends Observable {
     private ClientModelFacade(){
     }
 
-    /**
-     * @pre This class changes, and needs to update other classes that it holds.
-     * @post The class is added as an observer to this class.
-     * @param o a new class that will be updated when ClientModelFacade changes
-     */
     public void addNewObserver(Observer o){
         addObserver(o);
     }
 
-    /**
-     * @pre This class is a singleton.
-     * @post A single instance of this class is returned.
-     * @return a single instance of the ClientModelFacade class
-     */
     public synchronized ClientModelFacade getInstance(){
         return SINGLETON;
     }
 
-    /**
-     * @pre The player or games have changed.
-     * @post The observers of this class know that it has changed.
-     */
     public void changed(){
         setChanged();
         notifyObservers();
     }
 
-    /**
-     * @pre There are available games on the client that the player can join.
-     * @post All the available games on the client are returned.
-     * @return List of all the games that the client holds that the player can join.
-     */
     public GameList getAvailableGames() {
         return mAvailableGames;
     }
 
-    /**
-     * @pre There are available games for the player to join.
-     * @post Sets all the available games to the new list. Notifies the observers that the games have changed.
-     * @param games List of all the available games.
-     */
     public void setAvailableGames(GameList games) {
         mAvailableGames = games;
         changed();
     }
 
-    /**
-     * @pre The player is in at least one game.
-     * @post Returns a list of the games the player is currently a member of.
-     * @return List of all the games that the player is a part of.
-     */
     public GameList getMyGames() {
         return mMyGames;
     }
 
-    /**
-     * @pre The player has joined at least one game.
-     * @post Sets all the player's games to the new list. Notifies the observers that the player's games have changed.
-     * @param mMyGames List of all the games that the player is a part of.
-     */
     public void setMyGames(GameList mMyGames) {
         this.mMyGames = mMyGames;
         changed();
     }
 
-    /**
-     * @pre The player is playing a game.
-     * @post The game that the player is a part of is returned.
-     * @return The current Game that the player is playing.
-     */
     public GameModel getCurrentGame() {
         return mCurrentGame;
     }
 
-    /**
-     * @pre The player is currently not a member of any game.
-     * @post Sets the current game to the new game. Tracks the new game state and notifies obversers that the new game started.
-     * @param mCurrentGame is the current game the player has decided to play.
-     */
     public void setCurrentGame(GameModel mCurrentGame) {
         this.mCurrentGame = mCurrentGame;
         changed();
     }
 
-    /**
-     * @pre The player's information is stored in the model.
-     * @post The player is returned.
-     * @return The current player.
-     */
     public Player getUser() {
         return mUser;
     }
 
-    /**
-     * @pre The current player is done playing, and a new one is signing in.
-     * @post The new player is set up in the model. The observers are notified that the player is different.
-     * @param mUser is a new player object.
-     */
     public void setUser(Player mUser) {
         this.mUser = mUser;
         changed();
@@ -162,7 +99,8 @@ public class ClientModelFacade extends Observable {
     }
 
     //add a bank card to the player's hand
-    public void takeBankCard(TrainCard card) {
+    public void takeBankCard(int pos) {
+        TrainCard card = getBankTrainCard(pos);
         mUser.addBankCard(card);
         changed();
     }
@@ -203,10 +141,14 @@ public class ClientModelFacade extends Observable {
         changed();
     }
 
-    public TrainCard getBankTrainCard(int pos) {
-        return mBank.getTrainCard(pos);
+    public List<TrainCard> getAllBankTrainCards() {
+        TrainCard[] bankCards = mBank.getAvailableTrainCards();
+        List<TrainCard> cards = new ArrayList<>();
+        for(int i = 0; i<bankCards.length; i++) {
+            cards.add(bankCards[i]);
+        }
+        return cards;
     }
-
 
 
     //END PRESENTER CODE
@@ -232,4 +174,9 @@ public class ClientModelFacade extends Observable {
     }
 
     //END INITIALIZING CODE
+
+    private TrainCard getBankTrainCard(int pos) {
+        return mBank.getTrainCard(pos);
+    }
+
 }
--- app/src/test/java/goldenhammer/ticket_to_ride_client/model/ClientModelFacadeTest.java
+++ app/src/test/java/goldenhammer/ticket_to_ride_client/model/ClientModelFacadeTest.java
@@ -4,6 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,10 +15,33 @@ import static org.junit.Assert.*;
  */
 
 public class ClientModelFacadeTest {
+    private ClientModelFacade model;
+    final TrainCard card1 = new TrainCard(Color.ORANGE);
+    final TrainCard card2 = new TrainCard(Color.BLACK);
+    final TrainCard card3 = new TrainCard(Color.BLUE);
+
+    //Orange and Black
+    List<TrainCard> trainCards1 = new ArrayList<>(Arrays.asList(card1, card2));
+    //Blue
+    List<TrainCard> trainCards2 = new ArrayList<>(Arrays.asList(card3));
+    //All of them
+    List<TrainCard> trainCards3 = new ArrayList<>(Arrays.asList(card1, card2, card3));
+
+
     @Before
     public void setUp() throws Exception {
+        model = ClientModelFacade.SINGLETON;
+        Player jonny = new Player(new Username("jonny"), new Password("hithere"));
+        model.setUser(jonny);
 
+        TrainCard[] cards = new TrainCard[5];
+        cards[0] = card1;
+        cards[1] = card2;
+        cards[2] = card2;
+        cards[3] = card3;
+        cards[4] = card1;
 
+        model.setBankCards(cards);
     }
 
     @After
@@ -23,22 +50,77 @@ public class ClientModelFacadeTest {
     }
 
     @Test
-    public void getUserTrainCardsTest() throws Exception {
+    public void drawTrainCardsTest() throws Exception {
+        System.out.println("Testing adding Train Cards to the Player hand....");
+        model.drawTrainCards(trainCards1);
 
+        List<TrainCard> trainCard1Real = model.getUserTrainCards();
+
+        for(int i=0; i<trainCards1.size(); i++) {
+            System.out.println(trainCards1.get(i).toString() + " = " + trainCard1Real.get(i).toString());
+            assertEquals(trainCards1.get(i), trainCard1Real.get(i));
+        }
+
+        model.drawTrainCards(trainCards2);
+
+        List<TrainCard> trainCard2Expected = trainCards3;
+        List<TrainCard> trainCard2Real = model.getUserTrainCards();
+
+        for(int i=0; i<trainCard2Real.size(); i++) {
+            System.out.println(trainCard2Expected.get(i).toString() + " = " + trainCard2Real.get(i).toString());
+            assertEquals(trainCard2Expected.get(i), trainCard2Real.get(i));
+        }
     }
 
     @Test
-    public void getUserDestCardsTest() throws Exception {
+    public void removeTrainCardsTest() throws Exception {
+        System.out.println("Testing removing cards form the Player Hand...");
 
+        model.drawTrainCards(trainCards2);
+        model.removeTrainCards(trainCards2);
+
+        List<TrainCard> result1 = model.getUserTrainCards();
+        assertEquals(result1.size(), 0);
+
+        model.drawTrainCards(trainCards3);
+        model.removeTrainCards(trainCards2);
+
+        List<TrainCard> result2 = model.getUserTrainCards();
+
+        for(int i=0; i<result2.size(); i++) {
+            System.out.println(result2.get(i).toString()+ " = "+trainCards1.get(i).toString());
+            assertEquals(result2.get(i), trainCards1.get(i));
+        }
+
+        model.removeTrainCards(trainCards1);
+        assertEquals(model.getUserTrainCards().size(), 0);
+    }
+
+    @Test
+    public void takeBankCardTest() throws Exception {
+        System.out.println("Testing user taking a bank card...");
+
+        assertEquals(model.getUserTrainCards().size(), 0);
+
+        model.takeBankCard(0);
+        assertEquals(model.getUserTrainCards().size(), 1);
+        assertEquals(model.getUserTrainCards().get(0), card1);
+
+        assertEquals(model.getAllBankTrainCards().get(0), null);
+
+        model.takeBankCard(1);
+        model.takeBankCard(3);
+
+        List<TrainCard> result = model.getUserTrainCards();
+        for(int i=0; i<trainCards3.size(); i++) {
+            assertEquals(result.get(i), trainCards3.get(i));
+        }
     }
 
     @Test
     public void drawDestCardsTest() throws Exception {
+        System.out.println("Testing adding cards to the DrawnDestCard...");
 
     }
 
-    @Test
-    public void drawTrainCardsTest() throws Exception {
-
-    }
 }
