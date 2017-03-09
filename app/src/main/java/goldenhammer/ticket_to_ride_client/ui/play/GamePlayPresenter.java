8e0d5edb05c5ab85f77a51549e3a3177ba1e0514
mckrieger <mckean.krieger@byu.edu>
2017-03-08 21:35:36 -0700

    Drawing tracks

--- app/src/main/java/goldenhammer/ticket_to_ride_client/ui/play/GamePlayPresenter.java
+++ app/src/main/java/goldenhammer/ticket_to_ride_client/ui/play/GamePlayPresenter.java
@@ -16,6 +16,7 @@ import goldenhammer.ticket_to_ride_client.model.ClientModelFacade;
 import goldenhammer.ticket_to_ride_client.model.DestCard;
 import goldenhammer.ticket_to_ride_client.model.GameName;
 import goldenhammer.ticket_to_ride_client.model.Track;
+import goldenhammer.ticket_to_ride_client.model.TrainCard;
 import goldenhammer.ticket_to_ride_client.model.commands.Command;
 import goldenhammer.ticket_to_ride_client.model.commands.DrawDestCardsCommand;
 import goldenhammer.ticket_to_ride_client.model.commands.ReturnDestCardsCommand;
@@ -116,8 +117,16 @@ public class GamePlayPresenter implements Observer, IGamePlayPresenter {
 
     }
 
+
     protected void initializeHand() {
-        owner.initializeHand(model.getHand().getDrawnDestinationCards().getRemainingDestCards(),model.getHand());
+        owner.initializeHand(model.getHand());
+    }
+
+    protected void updateBank(){
+        if (model.getAllBankTrainCardsArray()!= null) {
+            owner.updateBank(model.getAllBankTrainCardsArray());
+        }
+
     }
 
     protected void updateMap() {
