44da0fafa95340a83ba5965d0b9470602b48e75e
Sean Beecroft <seanjib@gmail.com>
2017-03-09 18:44:01 -0700

    renamed the first four destination cards

---
+++ app/src/main/gen/goldenhammer/ticket_to_ride_client/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package goldenhammer.ticket_to_ride_client;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
